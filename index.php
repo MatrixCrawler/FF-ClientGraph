3d6cbf947ef036cb29adcea121c457d3617d4a01
Johannes Brunswicker <matrixcrawler@matrixcrawler.net>
2015-10-26 15:41:50 +0100

    * added index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>Feifunk - Clientgraph-Generator</title>
+</head>
+<body>
+<h1 align="center">Freifunk - Clientgraph-Generator</h1>
+Um einen Graphen für einen Node zu generieren benutze bitte folgende
+URL: <?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/graph.php?client=&lt;CLIENTID&gt;' ?>
+</body>
+</html>
